d314b99dc142433017fd09b14a29f61cc2fd37fa
Aleksey Borisov <attcsfn@gmail.com>
2025-06-10 23:31:21 +0300

    Created models Title, Category, Genre

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,63 @@
+from datetime import datetime
+
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=256,
+        unique=True,
+    )
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=256,
+        unique=True,
+    )
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField('Название', max_length=256)
+    year = models.IntegerField(
+        'Год выпуска',
+        validators=[
+            MinValueValidator(1000, message="Год должен быть не меньше 1000."),
+            MaxValueValidator(
+                datetime.now(), message='Год не может быть больше текущего')
+        ]
+    )
+    description = models.TextField('Описание', blank=True)
+    genre = models.ManyToManyField(
+        Genre,
+        related_name='titles',
+        verbose_name='Жанры'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.PROTECT,
+        related_name='titles',
+        verbose_name='Категории'
+    )
+
+    def __str__(self):
+        return self.name
