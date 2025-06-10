ff21643af048374642c81d1b09af380ba262a6cf
Aleksey Borisov <attcsfn@gmail.com>
2025-06-11 00:05:34 +0300

    Add models to adminka

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -43,7 +43,7 @@ class Title(models.Model):
         validators=[
             MinValueValidator(1000, message="Год должен быть не меньше 1000."),
             MaxValueValidator(
-                datetime.now(), message='Год не может быть больше текущего')
+                datetime.now().year, message='Год не может быть больше текущего')
         ]
     )
     description = models.TextField('Описание', blank=True)
