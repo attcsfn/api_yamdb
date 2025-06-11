7b4f8da48168c461acb1361dd5d73b1793d93a0c
Aleksey Borisov <attcsfn@gmail.com>
2025-06-11 07:02:58 +0300

    Created serializers.py

--- api_yamdb/api_v1/apps.py
+++ api_yamdb/api_v1/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ReviewsApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'reviews_api'
+    name = 'api_v1'
---
+++ api_yamdb/api_v1/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+
+from reviews.models import Category, Genre, Title
+
+
+class CategorySerializer(serializers.ModelSerializerd):
+
+    class Meta:
+        model = Category
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Genre
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    genre = SlugRelatedField(
+        slug_field='slug',
+        many=True,
+        read_only=True
+    )
+    category = SlugRelatedField(
+        slug_field='slug',
+        read_only=True
+    )
+
+    class Meta:
+        model = Title
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
+        read_only_fields = ('id',)
