2a0cdcf80c5b16f36c54d715b9aded7735a63f94
Aleksey Borisov <attcsfn@gmail.com>
2025-06-11 07:13:01 +0300

    Create view.py

--- api_yamdb/api_v1/views.py
+++ api_yamdb/api_v1/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.read_only=True
+from reviews.models import Category, Genre, Title
+from .serializers import (CategorySerializer, GenreSerializer, TitleSerializer)
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """API для работы с Категориями"""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class GenreViewSet(viewsets.ModelViewSet):
+    """API для работы с Жанрами"""
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """API для работы с Произведениями"""
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('api/v1/', include('api_v1.urls')),
+    path('api/v1/', include('api_v1.urls')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
