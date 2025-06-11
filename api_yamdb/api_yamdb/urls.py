d20387082141daedc4b3a758552197d6ec48f697
BelikovDenis <DenisBelikov1992@yandex.ru>
2025-06-11 15:23:23 +0300

    testing

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -3,11 +3,6 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-from rest_framework.routers import DefaultRouter  #  код для удаления
-from api.views import CommentViewSet  #  код для удаления
-
-router = DefaultRouter()  #  код для удаления
-router.register(r'comments', CommentViewSet)  #  код для удаления
 
 urlpatterns = [
     path('admin/', admin.site.urls),
