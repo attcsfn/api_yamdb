ef090f7950708b1b70513f463c06a19957cfa932
attcsfn <attcsfn@gmail.com>
2025-06-13 20:38:22 +0300

    Update settings.py
    Add default value EMAIL_HOST

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.yandex.ru')
 
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 
