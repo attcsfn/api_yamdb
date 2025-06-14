45d3e4044337841de9d67f584427a71359166799
Darya Boyarkina <bo_dary@mail.ru>
2025-06-14 13:18:25 +0700

    fix_2 settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -132,7 +132,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.yandex.ru')
 
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 
