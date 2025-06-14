12cf9c0f806aebefffa20f270090b39c59eb3b95
Darya Boyarkina <bo_dary@mail.ru>
2025-06-14 12:59:11 +0700

    fix settings.py

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -136,6 +136,6 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
